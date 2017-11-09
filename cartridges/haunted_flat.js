9a81fb398496f76d9cc51694b3db5ec09cac4e86
wemmm <jenny.wem@gmail.com>
2017-11-09 20:29:52 +0000

    Populates the hallway

--- cartridges/haunted_flat.js
+++ cartridges/haunted_flat.js
@@ -160,6 +160,13 @@ var gameData = {
 		'Hallway' : {
 			firstVisit : true,
 			description : '\nPLACEHOLDER because I have yet to write this.\n',
+			interactables : {
+				room : { look : '\nYou can see the following: a telephone, a mirror and the door to Berlioz\'s room.\n' },
+				telephone : { look : '\nA telephone. You really ought to phone Rimsky and ask for some more details about this ostensible contract with Woland.\n' },
+        mirror : { look : '\nAnother full length mirror hangs in your hallway.\n' },
+        self : { look : '\nYou do feel a little better, at least physically, and you are successfully wearing trousers, which represents something of an improvement to your situation. It\'s a shame about the existential dread you\'re now feeling, but you can\'t have it all.\n' },
+				door : { look : '\nThe door to Berlioz\'s room. It has a vast wax seal on it now, and panic grips you as you recall a potentially compromising conversation you had with him some days before.\n' }
+			},
 		},
 	}
 };
